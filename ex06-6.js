f46d89cc3c140525ea12dd6a6bfd019f466df27c
aoi-1205.peach <aoi-1205.peach@i.softbank.jp>
2018-10-21 21:56:09 -0400

    change table size

--- ex06-6.js
+++ ex06-6.js
@@ -1,10 +1,17 @@
-var who = 'world';
-function sayhello66() {
-alert('Hello, ' + who + 'が来た!');
+var who66='world';
+ // var come ='が来た'
+ function sayhello66(){
+   //var who='world';
+   alert('hello,'+ who66 +'!');
+ }
+function taro() {
+  who66='太郎';
 }
-function sayhello66() {
-alert('Hello, ' + who + '!');
+
+function hanako() {
+  who66='花子';
 }
-function someone(x) {
-who = x;
+
+function gakita() {
+  alert(who66 + '')
 }
